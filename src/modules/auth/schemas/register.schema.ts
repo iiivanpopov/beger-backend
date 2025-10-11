9ee349e737d799a5c22747f7c58f677912b30319
iiivanpopov <iiivanpopov999@gmail.com>
2025-10-11 12:54:45 +0300

    use unified validators for register

--- src/modules/auth/schemas/register.schema.ts
+++ src/modules/auth/schemas/register.schema.ts
@@ -1,11 +1,11 @@
 import type { InferOutput } from 'valibot'
-import { minLength, object, pipe, string } from 'valibot'
-import { config } from '@/config'
+import { object } from 'valibot'
+import { fullNameValidation, passwordValidation, userNameValidation } from '@/utils'
 
 export const RegisterBody = object({
-  userName: pipe(string(), minLength(config.validation.MIN_USERNAME_LEN)),
-  password: pipe(string(), minLength(config.validation.MIN_PASSWORD_LEN)),
-  fullName: pipe(string(), minLength(config.validation.MIN_FULLNAME_LEN)),
+  userName: userNameValidation,
+  password: passwordValidation,
+  fullName: fullNameValidation,
 })
 
 export type RegisterData = InferOutput<typeof RegisterBody>
