a94d56b20ecf368f5185ff310973c9299d17fd53
iiivanpopov <iiivanpopov999@gmail.com>
2025-10-12 10:28:54 +0300

    refactor

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,7 @@
 #!/bin/sh
 set -e
 
-run_migrations() {
-  echo "[INFO] Migrating database..."
-  bun db:migrate
-}
+bun run drizzle-kit migrate
+bun run seed:prod
 
-seed_database() {
-  echo "[INFO] Seeding database..."
-  bun seed:prod
-}
-
-start_app() {
-  echo "[INFO] Starting application..."
-  exec bun start:dist:prod
-}
-
-main() {
-  echo "[INFO] Starting backend..."
-  run_migrations
-  seed_database
-  start_app
-}
-
-main "$@"
+exec bun run dist:prod
